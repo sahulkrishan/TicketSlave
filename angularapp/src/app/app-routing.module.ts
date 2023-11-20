819568906af64bfdb3dded39d88fe8a951b8bd12
bonersupremacist <ishrathasrat@hotmail.com>
2023-11-20 15:11:22 +0000

    16-evenement bekijken - is enkel routing. werkend

---
+++ angularapp/src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from '@angular/router';
+import {EventDetailComponent} from "./event-detail/event-detail.component";
+import {EventOverviewComponent} from "./event-overview/event-overview.component";
+
+const routes: Routes = [
+  { path: '', component: EventOverviewComponent }, // Default route
+  { path: 'details', component: EventDetailComponent },
+  // Add more routes as needed
+];
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -8,13 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EventCardComponent } from './event-card/event-card.component';
 import { EventCarouselComponent } from './event-carousel/event-carousel.component';
 import {MatCardModule} from "@angular/material/card";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
-    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatCardModule, EventOverviewComponent, EventCardComponent, EventCarouselComponent,
+    BrowserModule, HttpClientModule, BrowserAnimationsModule, MatCardModule, EventOverviewComponent, EventCardComponent, EventCarouselComponent, AppRoutingModule
   ],
   providers: [],
   exports: [
