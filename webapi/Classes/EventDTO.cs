89b28ce978d4a7808a9337ff5a8f839a8f88708c
bonersupremacist <ishrathasrat@hotmail.com>
2023-11-21 16:07:43 +0000

    16-evenement bekijken - cypress installeert - db connectie gemaakt - delete post en get endpoints gemaakt

---
+++ webapi/Classes/EventDTO.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using webapi.Classes;
+
+namespace TicketSlave.Classes
+{
+    public class EventDTO
+    {
+        [Key]
+        [Required]
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Guid LocationId { get; set; }
+        public Guid CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<string> ImageUrls { get; set; }
+        public DateTime EventStartAt { get; set; }
+        public DateTime EventEndAt { get; set; }
+        public DateTime SaleStartAt { get; set; }
+        public DateTime SaleEndAt { get; set; }
+        public DateTime PresaleStartAt { get; set; }
+        public DateTime PresaleEndAt { get; set; }
+        public DateTime PresalePasswordHash { get; set; }
+        public Visibility Visibility { get; set; }
+    }
+
+}
--- webapi/Controllers/eventController.cs
+++ webapi/Controllers/eventController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using TicketSlave.Classes;
+using webapi.Classes;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace webapi.Controllers;
@@ -10,62 +12,102 @@ namespace webapi.Controllers;
 [Route("[controller]")]
 public class eventController : ControllerBase
 {
+    private readonly AppDbContext _context;
+    public eventController(AppDbContext context) {  _context = context; }
+
 
     [HttpGet]
-    public List<Event> Get()
+    public async Task<ActionResult<List<Event>>> GetAllEvents()
     {
-        List<Event> events = new List<Event>();
-        
-
-        Event test = new Event();
-        test.Id = Guid.NewGuid();
-        test.Title = "Test";
-        test.Description = "Test de beschrijving met deze tekst, want beschrijvingen moeten ook getoond worden ds ja dat een beetje";
-        test.ImageUrls = new List <string> { "https://th.bing.com/th/id/OIP.wbjIIP8bQudTnShXyFymDgHaE8?rs=1&pid=ImgDetMain" };
-
-        Event test_wee = new Event();
-        test_wee.Id = Guid.NewGuid();
-        test_wee.Title = "Test twee";
-        test_wee.Description = "Test twee de beschrijving met deze tekst, want beschrijvingen moeten ook getoond worden ds ja dat een beetje";
-        test_wee.ImageUrls = new List<string> { "https://th.bing.com/th/id/OIP.wbjIIP8bQudTnShXyFymDgHaE8?rs=1&pid=ImgDetMain" };
-
-        Event test_drie = new Event();
-        test_drie.Id = Guid.NewGuid();
-        test_drie.Title = "Test drie";
-        test_drie.Description = "Test drie de beschrijving met deze tekst, want beschrijvingen moeten ook getoond worden ds ja dat een beetje";
-        test_drie.ImageUrls = new List<string> { "https://th.bing.com/th/id/OIP.wbjIIP8bQudTnShXyFymDgHaE8?rs=1&pid=ImgDetMain" };
-        events.Add(test); 
-        events.Add(test_wee); 
-        events.Add(test_drie);
-        return events;
-
+        var foundedEvents = await _context.Events.ToListAsync();
+        if (foundedEvents.Any()) { 
+            return Ok(foundedEvents);
+        }
+        return BadRequest();
     }
     [HttpGet("{id}")]
-    public ActionResult<IEnumerable<Event>> GetEventById(string id)
+    public async Task<ActionResult<Event>> GetEventById(string id)
     {
         Guid resultGuid;
         if (Guid.TryParse(id, out resultGuid))
         {
-            Event test = new Event();
-            test.Id = resultGuid;
-            test.Title = "succes api with id works";
-            test.Description = "Test detail de beschrijving met deze tekst, want beschrijvingen moeten ook getoond worden ds ja dat een beetje";
-            test.ImageUrls = new List<string> { "https://th.bing.com/th/id/OIP.wbjIIP8bQudTnShXyFymDgHaE8?rs=1&pid=ImgDetMain" };
-            return Ok(test);
+           var foundedEvent =  await _context.Events.FindAsync(resultGuid);
+            if (foundedEvent != null)
+            {
+                return Ok(foundedEvent);
+            }
         }
-        else
+        return BadRequest();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Event>> createEvent([FromBody] EventDTO evenement) {
+
+        if (evenement == null)
         {
-            Event test = new Event();
-            test.Id = Guid.NewGuid();
-            test.Id = resultGuid;
-            test.Title = "succes api with id works";
-            test.Description = "Test";
-            test.ImageUrls = new List<string> { "https://th.bing.com/th/id/OIP.wbjIIP8bQudTnShXyFymDgHaE8?rs=1&pid=ImgDetMain" };
-            // The input string was not in a valid GUID format
-            return Ok(test);
+            return BadRequest();
+            
+        }
+        var location = await _context.Locations.FindAsync(evenement.LocationId);
+        if (location == null)
+        {
+            return NotFound("Location not found.");
+        }
+        var newEvent = new Event() {
+            Id = evenement.Id,
+            Title = evenement.Title,
+
+            Description = evenement.Description,
+            ImageUrls = evenement.ImageUrls,
+            CreatedAt = evenement.CreatedAt,
+            CreatedBy = evenement.CreatedBy,
+            LocationId = location.Id,
+            Location = location,
+            EventEndAt = evenement.EventEndAt,
+            EventStartAt    = evenement.EventStartAt,
+            SaleEndAt   = evenement.SaleEndAt,
+            SaleStartAt = evenement.SaleStartAt,
+            PresaleStartAt = evenement.PresaleStartAt,
+            PresaleEndAt = evenement.PresaleEndAt,
+            PresalePasswordHash = evenement.PresalePasswordHash,
+            Visibility = evenement.Visibility,
+        };
+        _context.Events.Add(newEvent);
+        try
+        {
+            await _context.SaveChangesAsync();
+            return Ok(newEvent);
+
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            return StatusCode(400, "Evenement maken is mislukt");
         }
 
-        
+    }
+    [HttpDelete("{id}")]
+
+    public async Task<ActionResult> delete(string id) {
+        Guid resultGuid;
+        if (Guid.TryParse(id, out resultGuid))
+        {
+            var foundedEvent = await _context.Events.FindAsync(resultGuid);
+            if (foundedEvent != null)
+            {
+                try
+                {
+                    var deletedEvent = _context.Events.Remove(foundedEvent);
+                    await _context.SaveChangesAsync();
+                    return Ok(deletedEvent);
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return StatusCode(777, "Evenement maken is mislukt");
+                }
+            }
+        }
+        return BadRequest();
+
     }
 }
 
