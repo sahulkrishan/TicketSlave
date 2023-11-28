36dc02943603a0cb7952331d0a8598bd8a71866d
Sahul Krishan <22118092+sahulkrishan@users.noreply.github.com>
2023-11-28 17:55:53 +0100

    develop: Update Href

--- angularapp/src/service/base.service.ts
+++ angularapp/src/service/base.service.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable, throwError} from "rxjs";
+import {environment} from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
-  protected baseApiUrl = './api/v1';
+  protected baseApiUrl = environment.baseHref + 'api/v1';
 
   constructor(protected http: HttpClient) {
   }
