0bf188609e51d489ce769cbb247c8705529481c7
SKRISHAN <sahulmehmi.krishan@metafoor.nl>
2023-12-06 19:08:27 +0100

    34: Added basic controller for Accounts

---
+++ webapi/Classes/UserDto.cs
@@ -0,0 +1,29 @@
+﻿namespace webapi.Classes;
+
+public class UserDto
+{
+  public string FirstName { get; set; }
+  public string LastName { get; set; }
+  public string? Email { get; set; }
+  public string? UserName { get; set; }
+  public List<ApplicationRoles> Roles { get; set; } = new();
+  public DateTime CreatedAt { get; set; }
+  public bool AcceptedTerms { get; set; }
+  public DateTime? DateAcceptedTerms { get; set; }
+  public bool TwoFactorEnabled { get; set; }
+
+  public static UserDto FromApplicationUser(ApplicationUser user)
+  {
+    return new UserDto
+    {
+      FirstName = user.FirstName,
+      LastName = user.LastName,
+      Email = user.Email,
+      UserName = user.UserName,
+      CreatedAt = user.CreatedAt,
+      AcceptedTerms = user.AcceptedTerms,
+      DateAcceptedTerms = user.DateAcceptedTerms,
+      TwoFactorEnabled = user.TwoFactorEnabled
+    };
+  }
+}
---
+++ webapi/Controllers/AccountController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using webapi.Classes;
+
+namespace webapi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("/api/v1/[controller]")]
+public class AccountController: ControllerBase
+{
+  private readonly AppDbContext _context;
+  private readonly UserManager<ApplicationUser> _userManager;
+  private readonly SignInManager<ApplicationUser> _signInManager;
+  
+  public AccountController(
+    AppDbContext context,
+    UserManager<ApplicationUser> userManager,
+    SignInManager<ApplicationUser> signInManager)
+  {
+    _context = context;
+    _userManager = userManager;
+    _signInManager = signInManager;
+  }
+  
+  [HttpGet]
+  [Route("profile")]
+  public async Task<ActionResult<UserDto>> GetCurrentUser()
+  {
+    var user = await _userManager.GetUserAsync(User);
+    if (user == null)
+      return NotFound();
+    var userDto = UserDto.FromApplicationUser(user);
+    return Ok(userDto);
+  }
+}
